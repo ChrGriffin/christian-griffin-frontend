ffc123f309d6b581d1dc075e5dc327e5768bcd64
Christian Griffin <christian.griffin@live.com>
2020-06-01 18:23:35 -0400

    install and use vue-match-heights

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import VueMatchHeights from 'vue-match-heights';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueMatchHeights);
 
 new Vue({
   router,
