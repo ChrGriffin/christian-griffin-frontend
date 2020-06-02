e6f87b37934a85155a5b9307dfdf382b78f5a4ef
Christian Griffin <christian.griffin@live.com>
2020-06-01 21:52:18 -0400

    test WorkExperienceItem

---
+++ tests/unit/WorkExperienceItem.spec.ts
@@ -0,0 +1,40 @@
+import { expect } from 'chai';
+import { shallowMount } from '@vue/test-utils';
+import WorkExperienceItem from '@/components/WorkExperienceItem.vue';
+import Job from '@/interfaces/Job';
+
+const job: Job = {
+  company: 'Witcher School of the Wolf',
+  url: 'https://witcher.rv',
+  description: 'Hmm.',
+  from: new Date('2012-01-01 00:00:00'),
+  to: new Date('2012-12-31 11:59:59'),
+};
+
+describe('WorkExperienceItem.vue', () => {
+
+  const workExperienceItem = shallowMount(WorkExperienceItem, {
+    propsData: { job },
+  });
+
+  it('renders company name and job description', () => {
+    expect(workExperienceItem.find('h4').exists()).to.equal(true);
+    expect(workExperienceItem.find('h4').text()).to.equal('Witcher School of the Wolf');
+
+    expect(workExperienceItem.find('p').exists()).to.equal(true);
+    expect(workExperienceItem.find('p').text()).to.equal('Hmm.');
+  });
+
+  it('displays a human-friendly date range', () => {
+    expect(workExperienceItem.find('h5').exists()).to.equal(true);
+    expect(workExperienceItem.find('h5').text()).to.equal('January 2012 - December 2012');
+  });
+
+  it('displays the date range as being to the present if `to` is null', () => {
+    const currentJob = Object.assign({}, job);
+    currentJob.to = null;
+    workExperienceItem.setProps({job: currentJob});
+
+    expect(workExperienceItem.find('h5').text()).to.equal('January 2012 - Present');
+  });
+});
