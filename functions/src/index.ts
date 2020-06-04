15280f3b68a436fcfec92a9fc0142fa64ef42c76
Christian Griffin <christian.griffin@live.com>
2020-06-04 16:02:49 -0400

    return transformed instagram posts from endpoint

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,10 +1,17 @@
-import { https, config } from 'firebase-functions';
+import {https, config} from 'firebase-functions';
 import axios from 'axios';
+import InstagramApiPost from './interfaces/InstagramApiPost';
+import InstagramPost from './interfaces/InstagramPost';
+import InstagramPostTransformer from './transformers/InstagramPostTransformer';
 
-export const getInstagramPosts = https.onRequest(async(request, response) => {
- await axios.get(
-     `https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink,media_type,thumbnail_url&access_token=${config().instagram.access_token}`
- );
+export const getInstagramPosts = https.onRequest(async (request, response) => {
+    const apiResponse = await axios
+        .get(`https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink,media_type,thumbnail_url&access_token=${config().instagram.access_token}`)
 
- response.send("Hello from Firebase!");
+    const transformer = new InstagramPostTransformer();
+    const posts: InstagramPost[] = apiResponse.data.data.map((post: InstagramApiPost): InstagramPost => {
+        return transformer.transform(post);
+    });
+
+    response.send(posts);
 });
--- functions/test/GetInstagramPosts.spec.ts
+++ functions/test/GetInstagramPosts.spec.ts
@@ -3,26 +3,37 @@ import { getInstagramPosts } from '../src';
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import flushPromises from 'flush-promises';
+
 const test = require('firebase-functions-test')();
+const instagramResponse = require('./fixtures/instagram_api_response.json');
 
 const axiosMock = new MockAdapter(axios);
 test.mockConfig({instagram: {access_token: 'geralt_of_rivia'}});
 
 describe('firebase.getInstagramPosts', () => {
 
-    it('makes a request to Christian\s Instagram page', async () => {
+    beforeEach(() => {
         axiosMock.onGet('https://graph.instagram.com/me/media?fields=id,caption,media_url,permalink,media_type,thumbnail_url&access_token=geralt_of_rivia')
-            .replyOnce(200);
+            .replyOnce(200, JSON.stringify(instagramResponse));
+    });
 
+    it('makes a request to my Instagram page', async () => {
         // @ts-ignore
         getInstagramPosts({}, { send: (content: string) => {}});
 
         await flushPromises();
-
         expect(axiosMock.history.get.length).to.equal(1);
+    });
+
+    it('returns a JSON response with Instagram posts', (done) => {
+        // @ts-ignore
+        getInstagramPosts({}, { send: (content: []) => {
+            expect(content).to.deep.equal(require('./fixtures/transformed_instagram_api_response.json'));
+            done();
+        }});
     });
 
     afterEach(() => {
         axiosMock.reset();
-    })
+    });
 });
