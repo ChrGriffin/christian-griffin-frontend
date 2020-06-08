3204f4e6c1dcea622a9fe80ecce1399a71a6126a
Christian Griffin <christian.griffin@live.com>
2020-06-07 21:10:16 -0400

    additional responsiveness in footer

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -2,13 +2,16 @@
     <footer class="flex">
         <img :src="require('./../assets/images/logo.png')" />
         <p>&copy; 2020 Christian Griffin</p>
-        <p class="flex">Powered by </p>
-        <a target="_blank" href="https://vuejs.org/">
-            <img class="powered-logo" :src="require('./../assets/images/vue_logo.png')" />
-        </a>
-        <a target="_blank" href="https://firebase.google.com/">
-            <img class="powered-logo" :src="require('./../assets/images/firebase_logo.png')" />
-        </a>
+        <p class="powered">
+            Powered by
+            <a target="_blank" href="https://vuejs.org/">
+                <img class="powered-logo" :src="require('./../assets/images/vue_logo.png')" />
+            </a>
+
+            <a target="_blank" href="https://firebase.google.com/">
+                <img class="powered-logo" :src="require('./../assets/images/firebase_logo.png')" />
+            </a>
+        </p>
     </footer>
 </template>
 
@@ -35,6 +38,7 @@
         img {
             height: $navbarLogoHeight;
             display: inline-block;
+            margin-bottom: -5px;
 
             &.powered-logo {
                 height: $navbarLogoHeight / 2;
@@ -48,6 +52,36 @@
             margin-left: 4rem;
             font-weight: bold;
             font-size: 1.2rem;
+        }
+
+        .powered {
+            display: flex;
+            align-items: center;
+        }
+    }
+
+    @media screen and (max-width: $mobileBreakpoint) {
+        footer {
+            img:not(.powered-logo) {
+                display: none;
+            }
+
+            p:not(.powered) {
+                margin-left: 0;
+            }
+        }
+    }
+
+    @media screen and (max-width: $tinyBreakpoint) {
+        footer {
+            p {
+                flex-basis: 100%;
+                margin-left: 0;
+
+                &.powered {
+                    margin-top: 1rem;
+                }
+            }
         }
     }
 </style>
