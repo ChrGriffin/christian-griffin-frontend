52b5f56bb5e9899b654172a46f7a1a188edef19c
Christian Griffin <christian.griffin@live.com>
2020-06-07 22:37:16 -0400

    use vue scrollto in app

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import VueMatchHeights from 'vue-match-heights';
+import VueScrollTo from 'vue-scrollto';
 
 Vue.config.productionTip = false;
 
 Vue.use(VueMatchHeights);
+Vue.use(VueScrollTo);
 
 new Vue({
   router,
