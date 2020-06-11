9da941d25d59a8d74b67e5e2568c079fa74d1c1c
Christian Griffin <christian.griffin@live.com>
2020-06-10 21:28:44 -0400

    remove parallax elements

--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -5,6 +5,8 @@
                 <h1>Hi, I'm Christian.</h1>
                 <h2>I'm a web developer with a passion for PHP, Laravel, Vue.js, and Typescript.</h2>
             </div>
+
+            <img :src="require('./../assets/images/header_profile.png')" />
         </div>
     </header>
 </template>
@@ -12,110 +14,91 @@
 <script lang="ts">
     import {Component, Vue} from 'vue-property-decorator';
 
-    @Component({
-        components: { Parallax },
-    })
+    @Component
     export default class SiteHeader extends Vue {
     }
 </script>
 
 <style scoped lang="scss">
     @import './../assets/scss/variables';
-
     header {
         background-color: $aqua;
         position: relative;
         overflow: hidden;
         padding-top: 4rem;
         padding-bottom: 4rem;
-
-        .container .title {
+        .container div {
             width: calc(50% - 2rem);
-
             h1 {
                 margin-top: 16rem;
                 font-size: 4rem;
                 letter-spacing: -2px;
                 font-weight: bold;
             }
-
             h2 {
                 margin-top: 1rem;
                 margin-bottom: 16rem;
                 font-size: 1.8rem;
             }
         }
-
-        .image {
+        img {
             position: absolute;
             right: 5%;
             z-index: 998;
-            width: 50%;
-
-            img {
-                height: 90%;
-                width: 90%;
-            }
         }
     }
-
     @media screen and (max-width: 1440px) {
         header {
-            .container .title {
+            .container div {
                 width: 50%;
             }
-
-            .image {
+            img {
                 right: 0;
             }
         }
     }
-
+    @media screen and (max-width: 1315px) {
+        header {
+            img {
+                right: -5%;
+            }
+        }
+    }
     @media screen and (max-width: 1200px) {
         header {
-            .container .title {
+            .container div {
                 width: 50%;
-
                 h1 {
                     font-size: 3rem;
                     margin-top: 12rem;
                 }
-
                 h2 {
                     font-size: 1.4rem;
                     margin-bottom: 12rem;
                 }
             }
-
-            .image {
+            img {
+                width: 40%;
                 right: 5%;
-                bottom: -75%;
-
-                img {
-                    height: 60%;
-                }
+                bottom: -15%;
             }
         }
     }
-
     @media screen and (max-width: $mobileBreakpoint) {
         header {
-            .container .title {
+            .container div {
                 width: 100%;
                 text-align: center;
-
                 h1 {
                     font-size: 3rem;
                     margin-top: 5rem;
                 }
-
                 h2 {
                     font-size: 1.4rem;
                     margin-bottom: 8rem;
                 }
             }
-
-            .image {
+            img {
                 display: none;
             }
         }
--- src/shims-modules.d.ts
+++ src/shims-modules.d.ts
@@ -1,3 +1,2 @@
 declare module 'vue-match-heights';
 declare module 'validate';
-declare module 'vue-parallaxy';
