3bb37da29baf804fd67423586b353d0d8bb8bcd7
Christian Griffin <christian.griffin@live.com>
2020-07-12 13:43:29 -0400

    fix incorrect class name

--- src/repositories/InstagramPostRepository.ts
+++ src/repositories/InstagramPostRepository.ts
@@ -1,7 +1,7 @@
 import InstagramPost from '@/interfaces/InstagramPost';
 import axios from 'axios';
 
-export default class PrintRepository {
+export default class InstagramPostRepositoryRepository {
 
     public async get(): Promise<InstagramPost[]> {
         const response = await axios.get('https://us-central1-christian-griffin-frontend.cloudfunctions.net/getInstagramPosts');
